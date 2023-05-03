6b6bcca177d6e13088c760d83c21942e4bcaa924
SharpDevOps10 <td090833@gmail.com>
2023-05-03 22:28:49 +0300

    Add `pbdSearchService` function

--- src/services.js
+++ src/services.js
@@ -7,3 +7,33 @@ const pdbEntryService = 'https://data.rcsb.org/rest/v1/core/entry/';
 const httpClient = (url, requestOption) => {
   return fetch(url, requestOption);
 };
+
+const pbdSearchService = () => {
+  const pbdSearchAbortController = new AbortController();
+  return (searchValue) => {
+    const encodedSearchValue = encodeURI(JSON.stringify(searchValue));
+    pbdSearchAbortController.abort();
+    const signal = pbdSearchAbortController.signal;
+    const url = pbdSearch + encodedSearchValue;
+
+    return new Promise((resolve, reject) => {
+      httpClient(url, {signal})
+        .then((response) => {
+          const contentType = response.headers.get('content-type');
+          if (response.ok && contentType && contentType.includes('application/json')) {
+            resolve(response.json());
+          } else {
+            const error = new Error('Failed to fetch search result');
+            error.status = response.status;
+            error.statusText = response.statusText;
+            reject(error);
+          }
+        })
+      .catch((error) => {
+        console.error(error);
+        reject(error);
+      });
+    });
+  };
+};
+
