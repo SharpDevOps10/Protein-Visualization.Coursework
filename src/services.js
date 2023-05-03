31e17351c8cdef6f1c56c9e471379ce69c21f314
SharpDevOps10 <td090833@gmail.com>
2023-05-03 21:27:20 +0300

    Add urls

---
+++ src/services.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const pbdSearch = 'https://search.rcsb.org/rcsbsearch/v1/query?json=';
+const modelServer = 'https://models.rcsb.org/v1/';
+const pdbEntryService = 'https://data.rcsb.org/rest/v1/core/entry/';
+
