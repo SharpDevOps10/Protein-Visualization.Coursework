517c02ae2cd33bdd26d03ee2047f86eab869b2c4
SharpDevOps10 <td090833@gmail.com>
2023-05-06 14:39:36 +0300

    Implement `getAtomCoordinates` function

---
+++ src/utils/structure.js
@@ -0,0 +1,17 @@
+'use strict';
+
+export const getAtomCoordinates = (structure) => {
+  if (typeof structure !== 'string') {
+    throw new Error('Invalid argument type. Expected string');
+  }
+  const firstAtomIndex = structure.indexOf('ATOM');
+  const atomLines = structure.substring(firstAtomIndex).split('\n');
+  let atomCoordinates = [];
+  if (atomLines && atomLines.length > 0) {
+    atomCoordinates = atomLines.filter((line) => {
+      const trimmedLine = line.trim();
+      return (/^ATOM \d* .* A 1$/).test(trimmedLine);
+    });
+  }
+  return atomCoordinates;
+}
