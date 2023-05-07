1c1a4bd4cfad8719e8e2ab669398a1a0482b4ec7
SharpDevOps10 <td090833@gmail.com>
2023-05-07 14:44:08 +0300

    Implement `memoize` function

--- src/utils/memoization.js
+++ src/utils/memoization.js
@@ -1,4 +1,16 @@
 'use strict';
 
-const getArgKey = (arg) => arg.toString() + ':' + typeof (arg);
+const getArgKey = (arg) => arg.toString() + ':' + typeof(arg);
 const generateKey = (args) => args.map(getArgKey).join('|');
+
+export const memoize = (fn) => {
+  const cache = Object.create(null);
+  return (...args) => {
+    const key = generateKey(args);
+    const value = cache[key];
+    if (value) return value;
+    const result = fn(...args);
+    cache[key] = result;
+    return result;
+  };
+};
