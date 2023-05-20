ddc2cf95048e8234f22e959d5abc5a52e5e58636
SharpDevOps10 <td090833@gmail.com>
2023-05-20 13:25:06 +0300

    Implement method that creates + returns a control for entering a range of values

--- src/components /graph-controls.js
+++ src/components /graph-controls.js
@@ -21,4 +21,29 @@ export default class GraphControls extends HTMLElement {
     minResGapInput.setAttribute('max', this._maxDistCutoff);
   }
 
+  createRangeInput(value, min, max, id, label, onChangeCallback) {
+    const rangeInput = document.createElement('input');
+    rangeInput.setAttribute('type', 'range');
+    rangeInput.setAttribute('value', value);
+    rangeInput.setAttribute('value', value);
+    rangeInput.setAttribute('min', (min || 0).toString());
+    rangeInput.setAttribute('max', (max || 0).toString());
+    rangeInput.setAttribute('step', 1);
+    rangeInput.setAttribute('id', id || 'range-id');
+
+    rangeInput.addEventListener('change', onChangeCallback);
+    const rangeValue = document.createElement('span');
+    rangeValue.setAttribute('id', id+'-value');
+    rangeValue.innerText = value;
+
+    const rangeLabel = document.createElement('label');
+    rangeLabel.setAttribute('id', id+"-wrapper");
+    rangeLabel.innerText = label;
+
+    rangeLabel.appendChild(rangeValue);
+    rangeLabel.appendChild(rangeInput);
+    return rangeLabel;
+
+  }
+
 }
