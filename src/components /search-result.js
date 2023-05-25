634ae89b715670010b8b0af1aaebe5d814644639
SharpDevOps10 <td090833@gmail.com>
2023-05-25 23:16:47 +0300

    Add Class that allows user to display a list of search results

---
+++ src/components /search-result.js
@@ -0,0 +1,63 @@
+'use strict';
+import { SearchResultItem } from './search-result-item';
+
+const componentHtml = document.createElement('template');
+componentHtml.innerHTML = `
+  <ul></ul>
+  <style>
+    ul {
+      position: absolute;
+      background-color: #ececec;
+      margin-top: 0;
+      width: 100%;
+    }
+    
+    li {
+      list-style-type: none;   
+      width: 80%;
+      margin: 1rem;
+      border: 1px solid #ccc;
+      display: inline-block;
+    }
+    li:hover {
+      background-color: #ccc;
+      cursor: pointer;
+    }
+    @media (min-width: 768px) {
+      li {
+        width: 15%;
+      }
+    }
+  </style>
+  `;
+
+export class SearchResult extends HTMLElement {
+  constructor() {
+    super();
+    this.appendChild(componentHtml.content);
+    this._resultList = [];
+    customElements.define('result-item', SearchResultItem, { extends: 'li' });
+
+    this.render();
+
+  }
+
+  get resultList() {
+    return this._resultList;
+  }
+
+  set resultList(results) {
+    this._resultList = results;
+    this.render();
+  }
+
+  renderResultItems() {
+    return this._resultList.map((resultText) => `<li is='result-item' text=${resultText}></li>`).join('');
+  }
+
+  render() {
+    this.querySelector('ul')
+      .innerHTML = `${this.renderResultItems()}`;
+  }
+
+}
