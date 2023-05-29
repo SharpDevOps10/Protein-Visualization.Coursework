cab1d3c2dcafc7ed8b5e5337eb3ee5c234ad5041
SharpDevOps10 <td090833@gmail.com>
2023-05-29 22:25:24 +0300

    Create `webpack-dev.config.js` file

---
+++ webpack-dev.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+const config = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    open: true,
+    static: './'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+};
+
+module.exports = config;
