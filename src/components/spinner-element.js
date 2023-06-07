9c1f9187d4c508ca66fa3cd58dab23763c27f1d1
SharpDevOps10 <td090833@gmail.com>
2023-06-07 10:20:34 +0300

    Implement updating to the inner HTML content of the `SpinnerElement`

---
+++ src/components/spinner-element.js
@@ -0,0 +1,40 @@
+'use strict';
+
+export class SpinnerElement extends HTMLElement {
+  constructor() {
+    super();
+    this.render();
+  }
+
+  render() {
+    this.innerHTML = this.hasAttribute('enabled') ? `
+      <style>
+        spinner-element {
+          position: absolute;
+          width: 100%;
+          height: 100%;        
+        }
+        #spinner-overlay {
+          background-color: white;
+          opacity: 0.7;
+          position: absolute;
+          width: 100%;
+          height: 100%;        
+        }
+        #spinner-wrapper {
+          position: absolute;
+          top: calc(50% - 2rem);
+          left: calc(50% - 2rem);
+          z-index: 1000;
+        }
+      </style>
+      <div id="spinner-wrapper">
+        <div class="spinner-border" role="status">
+          <span class="visually-hidden">Loading...</span>
+        </div>
+      </div>
+      <div id="spinner-overlay"></div>
+    ` : ``;
+  }
+
+}
