9c0cd40c23e894d594dd6a1ea29dc390800cf56d
SharpDevOps10 <td090833@gmail.com>
2023-06-07 20:59:00 +0300

    Add method that is used to display notifications

--- src/components/graph-app.js
+++ src/components/graph-app.js
@@ -30,6 +30,15 @@ export default class GraphApp extends HTMLElement {
 
   }
 
+
+  showAlert = (alertType, alertText) => {
+    const alertEl = this.shadowRootRef.querySelector('alert-element');
+    alertEl.alertText = alertText;
+    alertEl.alertType = alertType;
+    alertEl.enabled = true;
+  };
+
+
   render() {
     this.shadowRootRef.innerHTML = `
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
