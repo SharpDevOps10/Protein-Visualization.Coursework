7fdd63fefdd400dd48dda8e41c951dfca26a5a97
SharpDevOps10 <td090833@gmail.com>
2023-06-07 20:46:41 +0300

    Add `render` function

--- src/components/graph-app.js
+++ src/components/graph-app.js
@@ -23,5 +23,17 @@ export default class GraphApp extends HTMLElement {
 
   }
 
+  render() {
+    this.shadowRootRef.innerHTML = `
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+      <script src="path/to/local/library.js"></script>
+      <alert-element></alert-element>
+      <spinner-element></spinner-element>
+      <protein-structure></protein-structure>
+      <graph graph-width="${this.graphWidth}" graph-height="${this.graphHeight}"></graph>
+      <graph-controls></graph-controls>    
+    `;
+  }
+
 
 }
